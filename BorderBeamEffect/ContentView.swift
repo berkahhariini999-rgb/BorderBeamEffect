25ab4168a6c97f80be17944e3ca626b2e4e7a90e
berkahhariini999-rgb <berkahhariini999@gmail.com>
2026-07-03 09:29:35 +0700

    completed component apps

--- BorderBeamEffect/ContentView.swift
+++ BorderBeamEffect/ContentView.swift
@@ -10,12 +10,67 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            VStack(alignment: .leading, spacing: 25) {
+                TextField("Ask Anything...", text: .constant(""))
+                    .padding(.top, 8)
+                HStack(spacing: 20) {
+                    Button {
+                        
+                    } label: {
+                        Text("Name/Model Name")
+                            .font(.caption)
+                            .foregroundStyle(Color.primary.opacity(0.8))
+                            .padding(.vertical, 8)
+                            .padding(.horizontal, 15)
+                            .background(.fill, in: .capsule)
+                    }
+                    Spacer(minLength: 0)
+                    Group {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "cloud")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "mic")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.up")
+                                .frame(width:35, height: 35)
+    
+                                .background(.background, in: .circle)
+                        }
+                    }
+                    .foregroundStyle(Color.primary)
+                }
+            }
+            .padding(15)
+            .borderBeam(
+                border: .primary,
+              //  hideFadeBorder: true,
+                beam: [.green, .blue, .pink, .red, .indigo],
+                beamBlur: 15,
+                cornerRadius: 20,
+                isEnabled: true
+            )
+            .background(.gray.opacity(0.1), in: .rect(cornerRadius:20))
+            
+           
         }
         .padding()
+       
     }
 }
 
