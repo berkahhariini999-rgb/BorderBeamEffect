2176392fba79ff7dfdc60026e77f42655aba98d7
berkahhariini999-rgb <berkahhariini999@gmail.com>
2026-07-03 08:38:52 +0700

    Initial Commit

---
+++ BorderBeamEffect/BorderBeamEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  BorderBeamEffectApp.swift
+//  BorderBeamEffect
+//
+//  Created by Iqbal Alhadad on 03/07/26.
+//
+
+import SwiftUI
+
+@main
+struct BorderBeamEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
